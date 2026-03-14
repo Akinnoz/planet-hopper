c85c541a0fc5bfc9c2752561e0c1329de8ee793c
Akinnoz <switchxm@gmail.com>
2026-03-15 02:07:33 +0700

    add score system and UI display

--- PlanetHopper/Assets/Scripts/CollectItem.cs
+++ PlanetHopper/Assets/Scripts/CollectItem.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class CollectItem : MonoBehaviour
 {
+    public int scoreValue = 1;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            GameManager.instance.AddScore(scoreValue);
             Destroy(gameObject);
         }
     }
