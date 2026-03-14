656b3af6e0926663efc4f2e9332149d08b388fcb
Akinnoz <switchxm@gmail.com>
2026-03-15 02:28:06 +0700

    add Physic Material

---
+++ PlanetHopper/Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour
+{
+    public float gravityForce = 20f;
+
+    Rigidbody rb;
+    GameObject[] planets;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+    }
+
+    void FixedUpdate()
+    {
+        Transform nearestPlanet = GetNearestPlanet();
+
+        Vector3 direction = (nearestPlanet.position - transform.position).normalized;
+
+        rb.AddForce(direction * gravityForce);
+
+        transform.up = -direction;
+    }
+
+    Transform GetNearestPlanet()
+    {
+        Transform nearest = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject planet in planets)
+        {
+            float distance = Vector3.Distance(transform.position, planet.transform.position);
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = planet.transform;
+            }
+        }
+
+        return nearest;
+    }
+}
