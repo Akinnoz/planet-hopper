a07af61326a8c1e5c6bc3b68e60e60ea49bcc0a0
Akinnoz <switchxm@gmail.com>
2026-03-15 08:50:52 +0700

    add oxygen warning system

--- PlanetHopper/Assets/Scripts/OxygenSystem.cs
+++ PlanetHopper/Assets/Scripts/OxygenSystem.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -11,13 +11,21 @@ public class OxygenSystem : MonoBehaviour
 
     public Slider oxygenBar;
 
+    public Image warningOverlay;
+    //public AudioSource warningSound;
+
+    public float warningLevel = 60f;
+
     bool isGameOver = false;
 
     void Start()
     {
         oxygen = maxOxygen;
+
         oxygenBar.maxValue = maxOxygen;
         oxygenBar.value = oxygen;
+
+        warningOverlay.color = new Color(1, 0, 0, 0);
     }
 
     void Update()
@@ -28,17 +36,31 @@ public class OxygenSystem : MonoBehaviour
 
         oxygenBar.value = oxygen;
 
+        // เตือนตอน oxygen ต่ำ
+        if (oxygen <= warningLevel)
+        {
+            FlashWarning();
+
+            //if (!warningSound.isPlaying)
+                //warningSound.Play();
+        }
+
         if (oxygen <= 0)
         {
             GameOver();
         }
     }
 
+    void FlashWarning()
+    {
+        float alpha = Mathf.PingPong(Time.time * 2f, 0.5f);
+
+        warningOverlay.color = new Color(1, 0, 0, alpha);
+    }
+
     void GameOver()
     {
         isGameOver = true;
-
-        Debug.Log("Out of Oxygen!");
 
         SceneManager.LoadScene("LoseScene");
     }
